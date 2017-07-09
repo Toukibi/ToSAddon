9c980f58de1a18931365c2b685b6c77236b1b0c0
xZeroW <vic_marts@hotmail.com>
2017-07-09 05:15:13 -0300

    Update buffcounter.lua

--- BuffCounter/src/buffcounter.lua
+++ BuffCounter/src/buffcounter.lua
@@ -89,9 +89,9 @@ local ResText = {
 		  , Close = "{#666666}Close{/}"
 		},
 		Msg = {
-			UpdateFrmaePos = "Display position was reset"
+			UpdateFrmaePos = "Display position was reset."
 		  , EndDragAndSave = "Dragging ends. Save the current position."
-		  , CannotGetHandle = "Failed to get the display screen"
+		  , CannotGetHandle = "Failed to get the display screen."
 		},
 		InFrame = {
 			Myself = "T"
@@ -170,23 +170,23 @@ local Toukibi = {
 				InitMsg = "[Add-ons]" .. addonName .. verText .. " loaded!"
 			  , NoSaveFileName = "The filename of save settings is not specified."
 			  , HasErrorOnSaveSettings = "An error occurred while saving the settings."
-			  , CompleteSaveSettings = "Saving settings completed."
-			  , ErrorToUseDefaults = "Change to use default setting because of an error occurred while loading the settings."
-			  , CompleteLoadDefault = "An error occurred while loading the default settings."
-			  , CompleteLoadSettings = "Loading settings completed."
+			  , CompleteSaveSettings = "Settings saved."
+			  , ErrorToUseDefaults = "Using default settings because an error occurred while loading the settings."
+			  , CompleteLoadDefault = "Default settings loaded."
+			  , CompleteLoadSettings = "Settings loaded!"
 			},
 			Command = {
-				ExecuteCommands = "Command '{#333366}%s{/}' was called"
-			  , ResetSettings = "The setting was reset."
-			  , InvalidCommand = "Invalid command called"
-			  , AnnounceCommandList = "Please use [ %s ? ] To see the command list"
+				ExecuteCommands = "Command '{#333366}%s{/}' was called."
+			  , ResetSettings = "Settings reset."
+			  , InvalidCommand = "Invalid command called."
+			  , AnnounceCommandList = "Please use [ %s ? ] o see the command list."
 				},
 			Help = {
 				Title = string.format("{#333333}Help for %s commands.{/}", addonName)
 			  , Description = string.format("{#92D2A0}To change settings of '%s', please call the following command.{/}", addonName)
 			  , ParamDummy = "[paramaters]"
 			  , OrText = "or"
-			  , EnableTitle = "Available commands"
+			  , EnableTitle = "Commands available"
 			}
 		},
 		kr = {
@@ -319,7 +319,7 @@ local Toukibi = {
 	ChangeLanguage = function(self, Lang)
 		local msg;
 		if self.CommonResText[Lang] == nil then
-			msg = string.format("Sorry, '%s' does not implement '%s' mode.{nl}Language mode has not been changed from '%s'.", 
+			msg = string.format("Sorry, '%s' haven't implemented '%s' mode yet.{nl}Language mode has not been changed from '%s'.", 
 								addonName, Lang, Me.Settings.Lang);
 			self:AddLog(msg, "Warning", true, false)
 			return;
