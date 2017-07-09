59532ddc68c9948f8f26eb647e550077595377bf
xZeroW <vic_marts@hotmail.com>
2017-07-09 15:34:32 -0300

    Update buffcounter.lua

--- BuffCounter/src/buffcounter.lua
+++ BuffCounter/src/buffcounter.lua
@@ -179,7 +179,7 @@ local Toukibi = {
 				ExecuteCommands = "Command '{#333366}%s{/}' was called."
 			  , ResetSettings = "Settings reset."
 			  , InvalidCommand = "Invalid command called."
-			  , AnnounceCommandList = "Please use [ %s ? ] o see the command list."
+			  , AnnounceCommandList = "Please use [ %s ? ] to see the command list."
 				},
 			Help = {
 				Title = string.format("{#333333}Help for %s commands.{/}", addonName)
